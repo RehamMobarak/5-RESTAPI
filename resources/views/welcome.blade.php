236d160b3f7b58c946cd7008a628c2575c1d7f59
RehamMobarak <reham.a.mobarak@gmail.com>
2021-10-20 03:17:26 +0200

    add: item model, db, cont

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Simple API: itemapi</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                Simple API: itemapi
                 </div>
 
                 <div class="links">
